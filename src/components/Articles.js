f903cfa42dd2bca79708675a844f52d92da6b3d3
twan <twan.mulder@gmail.com>
2019-09-23 22:59:48 +0200

    New Medium widget added

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,35 +1,21 @@
 import React from 'react';
 
 class Articles extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasExecuted: false,
-    };
+  addButtons() {
+    let titles = document.querySelectorAll('.medium-widget-article__title');
   }
+
   componentDidMount() {
-    const script = document.createElement('script');
-    const element = document.querySelector('.rss');
-
-    script.src =
-      '//rss.bloople.net/?url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40toktoktwan&showicon=true&type=js';
-    script.async = true;
-
-    // Dit is dikke whack
-    if (!this.state.hasExecuted) {
-      Object.defineProperty(document, 'scripts', {
-        value: [script],
-      });
-      this.setState({
-        hasExecuted: true,
-      });
-    }
-
-    element.appendChild(script);
+    window.mediumWidget();
+    this.addButtons();
   }
 
   render() {
-    return <div className="rss" />;
+    return (
+      <div className="rss">
+        <div id="medium-widget" />
+      </div>
+    );
   }
 }
 
