f7934bdb306beed32408e20c42f04335475a7467
TwanMulder <twan.mulder@gmail.com>
2019-11-23 23:00:08 +0100

    Redid projects
    AddedKLM Blue Fly Days, removed someold projects

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,17 +1,27 @@
 import portfolio from 'images/portfolio.webp';
 import anytime from 'images/anytime.png';
-import neroa from 'images/neroa.webp';
 import potetos from 'images/potetos.webp';
 import reactcoin from 'images/reactcoin.webp';
 import universe from 'images/universe.webp';
-import cssdoodle from 'images/cssdoodle.webp';
 import stormdigital from 'images/stormdigital.png';
-import stormlogo from 'images/stormlogo.png';
 import klm from 'images/KLM.png';
+import zebra from 'images/zebra.png';
 
 // image can be any size. just make sure it is close to a 1:1 ratio - a square.
 
 export default [
+  {
+    title: 'Storm Digital',
+    description:
+      'At the moment I am employed at Storm Digital, where I work as a creative developer. I work with Greensock and Vue on a daily basis, building dynamic display advertisements. Besides creating these, I also help out our Conversion Rate Optimalitzation (CRO) team build their A/B tests.',
+    skills: ['HTML5', 'CSS3', 'Javascript', 'Vue', 'Greensock', 'CRO'],
+    image: stormdigital,
+    alt: 'storm digital',
+    links: {
+      github: 'https://github.com/jengelstengel/anytimers-temp',
+      preview: 'https://www.stormdigital.nl/',
+    },
+  },
   {
     title: 'KLM 100 Year Campaign',
     description:
@@ -24,25 +34,14 @@ export default [
     },
   },
   {
-    title: 'Storm Digital',
-    description: 'At the moment I am employed at Storm Digital, where I work as a creative developer. I work with Greensock and Vue on a daily basis, building dynamic display advertisements and creating intriguing campaigns using Google Doubleclick Studio.',
-    skills: ['HTML5', 'CSS3', 'Javascript', 'Vue', 'Greensock', 'Doubleclick Studio'],
-    image: stormdigital,
-    alt: 'storm digital',
+    title: 'KLM Blue Fly Days Campaign',
+    description:
+      'Every single year, KLM sets has their "Blue Fly Days" campaign. Storm Digitals "Creatives" team designs and builds the digital creatives for this campaign. In November of 2019, I developed the HomePage Take-Over (HPTO) for this campaign.<br /><br /> <span style="color:#bbb;">Source is private, sorry!</span>',
+    skills: ['HTML5', 'CSS3', 'Javascript', 'Greensock'],
+    image: zebra,
+    alt: 'zebras in a savannah',
     links: {
-      github: 'https://github.com/jengelstengel/anytimers-temp',
-      preview: 'https://www.stormdigital.nl/',
-    },
-  },
-  {
-    title: 'Animated Storm Digital Logo',
-    description: 'During my employment at Storm Digital, I started working with GSAP. To start developing my skills, I built a small script that animates and "draws" their logo. It is built with GSAP and DrawSVG, all in-line (for implementation reasons).',
-    skills: ['HTML5', 'CSS3', 'Javascript', 'Vue', 'Greensock', 'Doubleclick Studio'],
-    image: stormlogo,
-    alt: 'storm logo',
-    links: {
-      github: 'https://github.com/jengelstengel/stormlogo',
-      preview: 'https://storm-logo.netlify.com/',
+      preview: 'https://clients.weborama.nl/files/campaigns2/8461296286/190015/NL_nu.nl_201904_index.html',
     },
   },
   {
@@ -58,28 +57,6 @@ export default [
     },
   },
   {
-    title: 'React Coin',
-    description: 'React-Coin is a very simple React web app, which I made to develop my React skills. It includes using mutiple different components, calling a cryptocurrency API, and dealing with client/server side routing. It is deployed on Heroku.',
-    skills: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS', 'Node.JS', 'Heroku'],
-    image: reactcoin,
-    alt: 'React-Coin',
-    links: {
-      github: 'https://github.com/jengelstengel/React-coin',
-      preview: 'https://twan-react-coin.herokuapp.com/',
-    },
-  },
-  {
-    title: 'AEC',
-    description:
-      'Neroa and I teamed up to develop an online platform for local residents of Ameland to track their electricity and gas usage. So far, this project has enrolled in 30 households and is steadily growing. This was my first time working with mutiple corporate teams, which taught me a lot about workflow and release schedules. <br /><br /> <span style="color:#bbb;">Source is private, sorry!</span>',
-    skills: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS', 'NodeJS', 'Spring'],
-    image: neroa,
-    alt: 'Neroa',
-    links: {
-      preview: 'https://aec.neroa-power.com/',
-    },
-  },
-  {
     title: 'VV Potetos',
     description:
       'This case involved testing, redesigning, and again testing a website from a local soccer club from Groningen. The final design focused mainly on re-doing the information structure and refreshing the look of the website. This project was in co-op with Convident. <br /><br /> <span style="color:#bbb;">Source is private, sorry!</span>',
@@ -92,14 +69,14 @@ export default [
     },
   },
   {
-    title: 'CSS Doodle',
-    description: 'CSS Doodle is a web component which can be used to draw patterns inside of HTML without the use of any CSS files. For this doodle I set a grid and shape, after which I animated them and based the colour of the circles on the position inside the grid.',
-    skills: ['HTML5', 'CSS3', 'CSS Doodle'],
-    image: cssdoodle,
-    alt: 'CSS Doodle',
+    title: 'React Coin',
+    description: 'React-Coin is a very simple React web app, which I made to develop my React skills. It includes using mutiple different components, calling a cryptocurrency API, and dealing with client/server side routing. It is deployed on Heroku.',
+    skills: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS', 'Node.JS', 'Heroku'],
+    image: reactcoin,
+    alt: 'React-Coin',
     links: {
-      github: 'https://github.com/jengelstengel/doodle-testing',
-      preview: 'https://css-doodle.netlify.com/',
+      github: 'https://github.com/jengelstengel/React-coin',
+      preview: 'https://twan-react-coin.herokuapp.com/',
     },
   },
   {
