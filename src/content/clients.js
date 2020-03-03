c864523d35b00d56f3cf8bcb55a5696ac7f73dac
TwanMulder <twan.mulder@gmail.com>
2020-03-03 09:56:47 +0100

    Added clients to content folder
    Created for future update

---
+++ src/content/clients.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    client: 'Volvo',
+    image: 'google.nl',
+  },
+  {
+    client: 'Audi',
+    image: 'google.nl',
+  },
+  {
+    client: 'Volkswagen',
+    image: 'google.nl',
+  },
+  {
+    client: 'KLM',
+    image: 'google.nl',
+  },
+  {
+    client: 'ŠKODA',
+    image: 'google.nl',
+  },
+  {
+    client: 'SEAT',
+    image: 'google.nl',
+  },
+  {
+    client: 'Takeaway',
+    image: 'google.nl',
+  },
+  {
+    client: 'ICS',
+    image: 'google.nl',
+  },
+  {
+    client: 'ABN AMRO',
+    image: 'google.nl',
+  },
+];
