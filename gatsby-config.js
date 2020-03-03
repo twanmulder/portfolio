12fb4abff7a41575581f2dd7a24e4a0fcaf987cd
TwanMulder <twan.mulder@gmail.com>
2020-03-03 09:42:32 +0100

    Added proper siteUrl to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Twan Mulder | Front-end Developer based in NL`,
-    siteUrl: `https://www.example.com`,
+    siteUrl: `https://www.twanmulder.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
