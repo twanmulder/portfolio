0c5468034558b27481b9227583db93cff5ac2c66
TwanMulder <twan.mulder@gmail.com>
2020-04-10 10:26:57 +0200

    Updated hero information

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,13 @@ class Hero extends React.PureComponent {
       <div className="hero-container">
         <Typewriter className="hero-title" text="Twan Mulder" />
         <div className="hero-description">
-          I am a web-developer, currently employed at Storm Digital. I love building beautiful user experiences. <br />
+          I'm a Creative Developer and Technical CRO Consultant, currently employed at{' '}
+          <a href="https://www.stormdigital.nl/" target="_blank">
+            Storm Digital
+          </a>.
+          <br />
+          Building beautiful user experiences is what I love.
+          <br />
           Check out my <Scrollchor to="#projects">projects</Scrollchor> below.
         </div>
       </div>
