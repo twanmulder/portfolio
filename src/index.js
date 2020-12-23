a0afff8cef20fe5cab80e9f5e690683c92b6faa7
TwanMulder <twan.mulder@gmail.com>
2020-12-23 18:28:40 +0100

    add tailwind and CRA
    tailwind and create-react-app config and setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import './style';
-import App from './components/app';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
