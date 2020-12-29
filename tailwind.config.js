617b20a0d71e800221253c73fd3b2674dd9cc012
TwanMulder <twan.mulder@gmail.com>
2020-12-29 17:34:00 +0100

    Small fix to mobile hero sizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       sans: ["karla", "Oswald", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
     },
     extend: {
+      height: {
+        "19/20-screen": "95vh",
+      },
       zIndex: {
         "-1": "-1",
       },
