eabbc1c0f28416981cd60b4c200b6046a8f54856
twanmulder <twan.mulder@gmail.com>
2021-05-03 22:08:35 +0200

    add gradient to about image

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import twan from "../assets/twan_transparent.png"
 export default function About() {
   return (
     <section className="sticky self-start hidden top-20 md:block" style={{ maxWidth: "180px", marginTop: "75px" }}>
-      <div className="mb-8 overflow-hidden rounded-sm " style={{ backgroundColor: "#fec150" }}>
+      <div className="mb-8 overflow-hidden rounded-sm " style={{ background: "linear-gradient(45deg, #ff9633, #FFC300)" }}>
         <img src={twan} width="180px" height="180px" alt="headshot of Twan Mulder" />
       </div>
       <p className="mb-1 text-sm tracking-wide text-gray-500 uppercase">About</p>
